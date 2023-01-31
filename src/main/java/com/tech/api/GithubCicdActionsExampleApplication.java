9ead652b009e2b67eddfe9eea126b2006897273a
kailastech <kailashvannal@gmail.com>
2023-01-31 20:22:49 +0530

    first commit

---
+++ src/main/java/com/tech/api/GithubCicdActionsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.tech.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCicdActionsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsExampleApplication.class, args);
+	}
+
+}
